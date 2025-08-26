c6db5321dc8456a4550bddfed80ce726b8b7ffe8
Vishal <gvvishal95@gmail.com>
2025-08-26 23:07:30 +0530

    added index optimisation and sitemap

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,11 @@
 import type { Metadata } from 'next'
+import Image from 'next/image'
 import { GeistSans } from 'geist/font/sans'
 import { GeistMono } from 'geist/font/mono'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'Navratri ',
+  title: 'Navratri - Dandiya Beats 2k25',
   description: 'Created with Milaan Services',
   generator: 'Milaan Services',
 }
@@ -25,7 +26,12 @@ html {
 }
         `}</style>
       </head>
-      <body>{children}</body>
+      <body>
+        <header style={{ display: 'flex', alignItems: 'center', padding: '10px' }}>
+          <Image src="/Dandiya-beats.jpg" alt="Milaan Services Logo" width={120} height={40} />
+        </header>
+        {children}
+      </body>
     </html>
   )
 }
---
+++ next-sitemap.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  siteUrl: 'https://dandiabeats.milaanservices.com',
+  generateRobotsTxt: true,
+};
